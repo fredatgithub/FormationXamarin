0ebda0db616cfa9a9e01704ff355b139b5bb21ce
fred <domaine75@gmail.com>
2017-09-07 16:09:18 +0200

    Add files via upload

---
+++ Stage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemosXAM.FListView.Model
+{
+   public class Stage
+    {
+        public string NomStage { get; set; }
+        public string DescriptionStage { get; set; }
+        public string ImageStage { get; set; }                  
+    }
+}

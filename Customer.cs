a0ed9b6ca5fbc1bd3f67fef5315130df3d7d72ab
fred <domaine75@gmail.com>
2017-09-08 13:22:09 +0200

    Add files via upload

---
+++ Customer.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XF_Northwind.Model
+{
+  public class Customer
+    {
+       // [JsonProperty("odata.type")]
+        public string Type { set; get; }
+        public string CustomerID { get; set; }
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+        public string ContactTitle { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+    }
+
+    public class OData
+    {
+        [JsonProperty("odata.metadata")]
+        public string Metadata { get; set; }
+        public List<Customer> Value { get; set; }
+    }
+
+    //public class CustomerJson
+    //{
+    //    [JsonProperty("customer")]
+    //    public Customer Customer { get; set; }
+    //}
+}
